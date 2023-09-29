d20c949a90ae11850f9d6c0df44dbb0276a06090
Akash A <akashava50@gmail.com>
2023-09-29 16:42:09 +0530

    Removed proxy

--- src/actions/profile.js
+++ src/actions/profile.js
@@ -12,7 +12,9 @@ import {
 
 export const getCurrentProfile = () => async (dispatch) => {
   try {
-    const res = await axios.get('/api/profile/me');
+    const res = await axios.get(
+      'https://devconnector-7z7s.onrender.com/api/profile/me'
+    );
     dispatch({
       type: GET_PROFILE,
       payload: res.data,
@@ -31,7 +33,9 @@ export const getCurrentProfile = () => async (dispatch) => {
 export const getProfiles = () => async (dispatch) => {
   dispatch({ type: CLEAR_PROFILE });
   try {
-    const res = await axios.get('/api/profile');
+    const res = await axios.get(
+      'https://devconnector-7z7s.onrender.com/api/profile'
+    );
     dispatch({
       type: GET_PROFILES,
       payload: res.data,
@@ -49,7 +53,9 @@ export const getProfiles = () => async (dispatch) => {
 
 export const getProfileById = (userId) => async (dispatch) => {
   try {
-    const res = await axios.get(`/api/profile/user/${userId}`);
+    const res = await axios.get(
+      `https://devconnector-7z7s.onrender.com/api/profile/user/${userId}`
+    );
     dispatch({
       type: GET_PROFILE,
       payload: res.data,
@@ -67,7 +73,9 @@ export const getProfileById = (userId) => async (dispatch) => {
 
 export const getGithubRepos = (username) => async (dispatch) => {
   try {
-    const res = await axios.get(`/api/profile/github/${username}`);
+    const res = await axios.get(
+      `https://devconnector-7z7s.onrender.com/api/profile/github/${username}`
+    );
     dispatch({
       type: GET_REPOS,
       payload: res.data,
@@ -93,7 +101,11 @@ export const createProfile =
         },
       };
 
-      const res = await axios.post('/api/profile', formData, config);
+      const res = await axios.post(
+        'https://devconnector-7z7s.onrender.com/api/profile',
+        formData,
+        config
+      );
 
       dispatch({
         type: GET_PROFILE,
@@ -129,7 +141,11 @@ export const addExperience = (formData, navigate) => async (dispatch) => {
       },
     };
 
-    const res = await axios.put('/api/profile/experience', formData, config);
+    const res = await axios.put(
+      'https://devconnector-7z7s.onrender.com/api/profile/experience',
+      formData,
+      config
+    );
 
     dispatch({
       type: UPDATE_PROFILE,
@@ -161,7 +177,11 @@ export const addEducation = (formData, navigate) => async (dispatch) => {
       },
     };
 
-    const res = await axios.put('/api/profile/education', formData, config);
+    const res = await axios.put(
+      'https://devconnector-7z7s.onrender.com/api/profile/education',
+      formData,
+      config
+    );
 
     dispatch({
       type: UPDATE_PROFILE,
@@ -187,7 +207,9 @@ export const addEducation = (formData, navigate) => async (dispatch) => {
 
 export const deleteExperience = (id) => async (dispatch) => {
   try {
-    const res = await axios.delete(`/api/profile/experience/${id}`);
+    const res = await axios.delete(
+      `https://devconnector-7z7s.onrender.com/api/profile/experience/${id}`
+    );
     dispatch({
       type: UPDATE_PROFILE,
       payload: res.data,
@@ -206,7 +228,9 @@ export const deleteExperience = (id) => async (dispatch) => {
 
 export const deleteEducation = (id) => async (dispatch) => {
   try {
-    const res = await axios.delete(`/api/profile/education/${id}`);
+    const res = await axios.delete(
+      `https://devconnector-7z7s.onrender.com/api/profile/education/${id}`
+    );
     dispatch({
       type: UPDATE_PROFILE,
       payload: res.data,
@@ -226,7 +250,7 @@ export const deleteEducation = (id) => async (dispatch) => {
 export const deleteAccount = () => async (dispatch) => {
   if (window.confirm('Are you sure?This cannot be undone')) {
     try {
-      await axios.delete('/api/profile');
+      await axios.delete('https://devconnector-7z7s.onrender.com/api/profile');
       dispatch({ type: CLEAR_PROFILE });
       dispatch({
         type: ACCOUNT_DELETED,
